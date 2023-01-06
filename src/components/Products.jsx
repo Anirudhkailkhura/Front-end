cceb66bfb4ad7b9183596b4a1edc32a953849d8a
Anirudh <107099444+Anirudhkailkhura@users.noreply.github.com>
2023-01-06 22:43:53 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -17,7 +17,7 @@ const Products = ({ cat, filters, sort }) => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get(
+        const res = await axios.post(
           cat
             ? `https://dhaarmik-zj46.onrender.com/api/products?category=${cat}`
             : "https://dhaarmik-zj46.onrender.com/api/products"
