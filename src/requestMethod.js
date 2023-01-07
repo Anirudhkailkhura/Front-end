0a029520db69c4d4c6a7711837bf15479ed107de
Anirudh <107099444+Anirudhkailkhura@users.noreply.github.com>
2023-01-07 23:00:13 +0530

    Update requestMethod.js

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://dhaarmik-zj46.onrender.com/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWFiN2RhMDM3MTk5ZTcwNjBiYzVmZSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NzE3ODM0OTEsImV4cCI6MTY3MjA0MjY5MX0.dcJWE5HKhsVffq8Sbl7H7rpy6rHq2yTOrfsUb8V7gKw"
 
 export const publicRequest = axios.create({
